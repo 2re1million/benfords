9b074a8b23b6f4cd2ffb5521aee2554d7ebc4568
2re1million <56553678+2re1million@users.noreply.github.com>
2023-10-08 01:32:48 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ import re
 from collections import defaultdict
 import math
 import pandas as pd
-import numpy as np
 import altair as alt
 from scipy.stats import chisquare
 
@@ -21,33 +20,21 @@ def benford_analysis(numbers):
         leading_digit = int(str(num)[0])
         leading_digits[leading_digit] += 1
 
-    total_numbers = sum(leading_digits.values())
+    total_numbers = len(numbers)
     benford_distribution = {i: math.log10(1 + 1/i) for i in range(1, 10)}
-    
+
     observed = [leading_digits[i] for i in range(1, 10)]
-    expected = [benford_distribution[i] * len(numbers) for i in range(1, 10)]
+    expected = [benford_distribution[i] * total_numbers for i in range(1, 10)]
     return observed, expected, leading_digits
 
-def benford_comment(observed_counts, expected_proportions):
-    # Ensure observed counts only include counts for digits 1 through 9
-    observed_counts = observed_counts[1:10] if len(observed_counts) == 10 else observed_counts
-
-    # Convert expected proportions to expected counts
-    total_numbers = sum(observed_counts)
-    expected_counts = [e * total_numbers for e in expected_proportions]
-
-    # Perform chi-squared test
+def benford_comment(observed_counts, expected_counts):
     chi2_stat, p_val = chisquare(observed_counts, expected_counts)
-    
-    # Comment based on p-value (assuming significance level of 0.05)
     if p_val < 0.05:
         return "Red Flag: The distribution deviates significantly from Benford's Law."
     else:
         return "The distribution is consistent with Benford's Law."
 
-
 st.title("Benford's Law Analysis")
-
 uploaded_file = st.file_uploader("Upload a PDF file", type="pdf")
 
 if uploaded_file:
@@ -56,31 +43,26 @@ if uploaded_file:
     
     if numbers:
         observed, expected, leading_digits_count = benford_analysis(numbers)
-        
-        # Compute total counts for observed and expected for chi-squared test
-        total_observed_counts = [count for count in leading_digits_count.values()]
-        total_expected_counts = [e * sum(total_observed_counts) for e in expected]
-        
+
         # Preparing data for Streamlit's bar chart with trendline
         df_chart = pd.DataFrame({
             'Digit': list(range(1, 10)),
             'Observed': observed,
-            'Expected': expected
+            'Expected (red)': expected
         })
-        
-        bars = alt.Chart(df_chart).mark_bar().encode(
+
+        bars = alt.Chart(df_chart).mark_bar(color='blue').encode(
             x='Digit:N',
-            y='Observed:Q',
-            color=alt.value('blue')
+            y='Observed:Q'
         )
         
         line = alt.Chart(df_chart).mark_line(color='red').encode(
             x='Digit:N',
-            y='Expected:Q'
+            y='Expected (red):Q'
         )
         
-        st.altair_chart(bars + line, use_container_width=True)
-        
+        st.altair_chart((bars + line).properties(width=600), use_container_width=True)
+
         # Displaying the count of leading digits
         df_count = pd.DataFrame({
             'Digit': list(range(1, 10)),
@@ -91,7 +73,7 @@ if uploaded_file:
         st.dataframe(df_count.set_index('Digit'))
         
         # Providing a comment based on chi-squared test result
-        comment = benford_comment(total_observed_counts, total_expected_counts)
+        comment = benford_comment(observed, expected)
         st.write(comment)
         
     else:
