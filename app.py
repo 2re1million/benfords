0eeb88d82de732f20a9371f7f9f293d738186e7e
2re1million <56553678+2re1million@users.noreply.github.com>
2023-10-08 01:52:10 +0200

    Update app.py

--- app.py
+++ app.py
@@ -31,6 +31,11 @@ def benford_analysis(numbers):
 st.title("Benford's Law Analysis")
 uploaded_file = st.file_uploader("Upload a PDF file", type="pdf")
 
+        st.info("""💡 Benford's Law is a surprising, yet frequently observed phenomenon about the distribution of leading digits in many sets of numerical data. 
+        According to this law, in many naturally occurring datasets, the leading digit is likely to be small. 
+        For example, the number 1 appears as the leading digit about 30% of the time, while 9 appears as the leading digit less than 5% of the time.""")
+        st.write("Count of leading digits:")
+
 if uploaded_file:
     st.write("Analyzing the document ...")
     numbers = extract_numbers_from_pdf(uploaded_file)
@@ -62,10 +67,7 @@ if uploaded_file:
             'Digit': list(range(1, 10)),
             'Count': [leading_digits_count[i] for i in range(1, 10)]
         })
-        st.info(""""💡 Benford's Law is a surprising, yet frequently observed phenomenon about the distribution of leading digits in many sets of numerical data. 
-        According to this law, in many naturally occurring datasets, the leading digit is likely to be small. 
-        For example, the number 1 appears as the leading digit about 30% of the time, while 9 appears as the leading digit less than 5% of the time.""")
-        st.write("Count of leading digits:")
+
         st.dataframe(df_count.set_index('Digit'))
         
     else:
